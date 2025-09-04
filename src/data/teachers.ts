8324ae972e879e33a8eb08fb3565d2407ecbc18f
Anirban Ray <136434130+AnirbanRay20@users.noreply.github.com>
2025-09-04 16:28:26 +0530

    Update teachers.ts

--- src/data/teachers.ts
+++ src/data/teachers.ts
@@ -1,6 +1,7 @@
 // Pre-approved teachers list
 export const approvedTeachers = [ 
-    "Dr. Pranati Rakshit", 
+    "Dr. Pranati Rakshit",
+    "Dr. Amrita Namatirtha",
     "Jayshree Bhattacharya", 
     "Bikramjit Sarkar", 
     "Sumanta Chatterjee", 
